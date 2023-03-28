e79347bbd0015ff9b3fcec8b6cec295a68f210a0
neela <nusratneela27@gmail.com>
2023-03-29 00:05:37 +0600

    retrive product from stored cart

--- ema-john-simple/src/components/Shop/Shop.jsx
+++ ema-john-simple/src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -13,9 +14,30 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, []);
 
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+        // step - 1: get id
+        for (const id in storedCart) {
+            // step- 2: get quantity of the product by using id
+            const addedProduct = products.find(product => product.id === id);
+            if (addedProduct) {
+                // step- 3: get quantity of the product
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity
+                // step- 4: add the added product to the saved cart
+                savedCart.push(addedProduct)
+
+            }
+        }
+        // step-5: save the cart
+        setCart(savedCart)
+    }, [products])
+
     const handleAddCart = (product) => {
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id)
     }
 
     return (
